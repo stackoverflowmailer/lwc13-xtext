84fc0edca327818b8378ce500bb37fa0a1270ff0
Karsten Thoms <karsten.thoms@itemis.de>
2013-02-26 07:52:47 +0100

    Add library project

---
+++ projects/org.eclipse.xtext.example.ql.lib/src/types/Money.java
@@ -0,0 +1,32 @@
+package types;
+
+import java.math.BigDecimal;
+
+public class Money {
+  private BigDecimal amount;
+
+  public Money(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  // Implement operators
+  public Money operator_minus(Money other) {
+    return new Money(this.amount.subtract(other.amount));
+  }
+
+  public Money operator_plus(Money other) {
+    return new Money(this.amount.add(other.amount));
+  }
+
+  public Money operator_multiply(Money other) {
+    return new Money(this.amount.multiply(other.amount));
+  }
+
+  public Money operator_divide(Money other) {
+    return new Money(this.amount.divide(other.amount));
+  }
+}
