fc5f01a9b8254286460ea9c3fd860035502c0e69
Karsten Thoms <karsten.thoms@itemis.de>
2013-03-08 06:57:41 +0100

    Formatter settings Format on save Sources formatted
    Signed-off-by: Karsten Thoms <karsten.thoms@itemis.de>

--- examples/JSF-QL/src/types/Money.java
+++ examples/JSF-QL/src/types/Money.java
@@ -3,34 +3,34 @@ package types;
 import java.math.BigDecimal;
 
 public class Money {
-	private BigDecimal amount;
+  private BigDecimal amount;
 
-	public Money(BigDecimal amount) {
-		this.amount = amount;
-	}
+  public Money(BigDecimal amount) {
+    this.amount = amount;
+  }
 
-	public BigDecimal getAmount() {
-		return amount;
-	}
+  public BigDecimal getAmount() {
+    return amount;
+  }
 
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
 
-	// Implement operators
-	public Money operator_minus(Money other) {
-		return new Money(this.amount.subtract(other.amount));
-	}
+  // Implement operators
+  public Money operator_minus(Money other) {
+    return new Money(this.amount.subtract(other.amount));
+  }
 
-	public Money operator_plus(Money other) {
-		return new Money(this.amount.add(other.amount));
-	}
+  public Money operator_plus(Money other) {
+    return new Money(this.amount.add(other.amount));
+  }
 
-	public Money operator_multiply(Money other) {
-		return new Money(this.amount.multiply(other.amount));
-	}
+  public Money operator_multiply(Money other) {
+    return new Money(this.amount.multiply(other.amount));
+  }
 
-	public Money operator_divide(Money other) {
-		return new Money(this.amount.divide(other.amount));
-	}
+  public Money operator_divide(Money other) {
+    return new Money(this.amount.divide(other.amount));
+  }
 }
